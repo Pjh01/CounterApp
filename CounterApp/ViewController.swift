362581b41d657289393daecb6cb69043512063ea
Pjh01 <estelle0102@naver.com>
2025-05-16 17:46:16 +0900

    Feat: plus / minus / reset 버튼 이벤트 구현

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,14 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var count = 0
     @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         countLabel.text = "\(count)"
     }
 
-
+    @IBAction func plusCount(_ sender: UIButton) {
+        count += 1
+        countLabel.text = "\(count)"
+    }
+    
+    @IBAction func minusCount(_ sender: UIButton) {
+        count -= 1
+        countLabel.text = "\(count)"
+    }
+    
+    @IBAction func resetCount(_ sender: UIButton) {
+        count = 0
+        countLabel.text = "\(count)"
+    }
 }
 
