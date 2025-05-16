65ea19fbb90dc7af7b50917b37706557705964dd
Pjh01 <estelle0102@naver.com>
2025-05-16 17:53:55 +0900

    Feat: count 범위 설정

--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -10,27 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
     
     var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
         countLabel.text = "\(count)"
+        plusButton.isEnabled = count < 10
+        minusButton.isEnabled = count > -10
     }
 
     @IBAction func plusCount(_ sender: UIButton) {
         count += 1
-        countLabel.text = "\(count)"
+        updateUI()
     }
     
     @IBAction func minusCount(_ sender: UIButton) {
         count -= 1
-        countLabel.text = "\(count)"
+        updateUI()
     }
     
     @IBAction func resetCount(_ sender: UIButton) {
         count = 0
-        countLabel.text = "\(count)"
+        updateUI()
     }
 }
 
